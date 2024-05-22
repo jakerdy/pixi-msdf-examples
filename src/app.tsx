49bc979818129f8712ab34f9b79152fd600ddfa1
Jak Erdy - Home <jakerdy@je.xyz>
2024-05-22 23:22:34 +0700

    [Viewport] [Feature] Add devtools binder button

--- src/app.tsx
+++ src/app.tsx
@@ -79,6 +79,7 @@ function ExamplesView()
 
         return <div class={st.viewport} ref={root}>
             <h4>Pixi Version: {PixiV7.VERSION}</h4>
+            <DevTools app={apps.v7} />
         </div>
     }
 
@@ -91,6 +92,7 @@ function ExamplesView()
 
         return <div class={st.viewport} ref={root}>
             <h4>Pixi Version: {PixiV8.VERSION}</h4>
+            <DevTools app={apps.v8} />
         </div>
     }
 
@@ -129,3 +131,12 @@ function ExamplesView()
 }
 
 
+function DevTools(opt: { app: any })
+{
+    return <button
+        class={st.dev_tools_btn}
+        onclick={() => (globalThis as any)['__PIXI_APP__'] = opt.app}
+    >
+        Dev Tools
+    </button>
+}
